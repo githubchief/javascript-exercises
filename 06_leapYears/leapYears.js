71ab848568690d66bce1e7ee02c13b874f363895
githubchief <ranganathanshams@gmail.com>
2022-11-29 19:02:30 +0530

    add solutions to js exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let leapYear=true;
 
+    if ((year%100 == 0 ) && (year%4 == 0))
+        {
+            if (year%400 == 0)
+            {
+                return leapYear
+            }
+            else return false;
+        }
+
+    if(year%4 == 0) {
+          return leapYear;
+    } 
+    return false;
 };
 
 // Do not edit below this line
