67ab348b12c3967b2dcf758f554572ef3c5575a7
githubchief <ranganathanshams@gmail.com>
2022-11-29 16:16:58 +0530

    add solutions to js excercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(srcArray,...delArray) {
+    
+    let temp1=srcArray.length;
+    let temp2=delArray.length;
+    for (let i=0; i<temp1;i++) {
+        for(let j=0; j<temp2;j++) {
+            if(srcArray[i]===delArray[j]) {
+                srcArray.splice(i,1);
+                i=0;
+            }
+        }
+    }
+    console.log(srcArray);
+    return (srcArray);
 };
 
 // Do not edit below this line
