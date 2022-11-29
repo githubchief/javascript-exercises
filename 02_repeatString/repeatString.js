6e91c4d58e2729f3b0e7a7dc42af29f759a7b55d
githubchief <ranganathanshams@gmail.com>
2022-11-29 12:01:02 +0530

    add solutions to js excerise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,24 @@
-const repeatString = function() {
+let count=0;
+let str="";
+const repeatString = function (str,count) {
 
+    let output='';
+
+    if (count<0) {
+        return "ERROR";
+    }
+    if (count==0) {
+        return '';
+    }
+    if (str=='') {
+        return '';
+    }
+    else {
+    for (let i=0;i<count;i++)
+     output+=str;
+    return output;
+    }
+    
 };
 
 // Do not edit below this line
