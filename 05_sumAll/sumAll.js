9706e97974e43b25e9249d2a5c39288c3c312d0e
githubchief <ranganathanshams@gmail.com>
2022-11-29 17:38:12 +0530

    add solution to js excercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,33 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum=0;
+    
+    if((typeof(num1)!==typeof(num2)))
+        return "ERROR";
 
+    if (((num1 < 0) || (num2 < 0) )) {
+        return "ERROR";
+    }
+    else if (num1<num2){
+
+        do {
+            sum=sum+num1;
+            num1++;
+        }while(num1!=num2);
+
+        return (sum+num2);
+    }
+    else if (num1 > num2) {
+        
+        do {
+            sum=sum+num2;
+            num2++;
+        }while(num1!=num2);
+
+        return (sum+num1);
+
+    }
 };
 
+//console.log(sumAll(1,4));
 // Do not edit below this line
 module.exports = sumAll;
