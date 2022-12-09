6a8245df3db59f8b3d51a665154873bf5ac1161a
githubchief <ranganathanshams@gmail.com>
2022-12-09 15:50:39 +0530

    add solutions to palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let temp=str.toLowerCase();
+  temp=temp.replace(/[^A-Za-z]/g, '');
+  temp=temp.replace(/\s/g,'');
+    let i=0;
+    let len=temp.length;
+    console.log(temp);
+    let j=len;
+  
+  while(i<len/2) {
+    if (temp[i]!=temp[j-1]) { 
+        
+        return false; 
 
+    } 
+    i++;
+    j--;
+        
+  }
+  return true;
 };
 
 // Do not edit below this line
