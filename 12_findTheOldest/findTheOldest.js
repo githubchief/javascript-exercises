6ac6d7bac0ac582a96ac60ffb3a1023a8a38e9ce
githubchief <ranganathanshams@gmail.com>
2022-12-09 21:01:01 +0530

    add full sol to findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,19 +1,37 @@
-const findTheOldest = function(people) {
-    return people.reduce((oldestPerson, person) => {
-        // If the current person doesn't have a year of death, calculate their age as of today
-        if (!(person.yearOfDeath)) {
+// const findTheOldest = function(people) {
+//     return people.reduce((oldestPerson, person) => {
+//         // If the current person doesn't have a year of death, calculate their age as of today
+//         if (!(person.yearOfDeath)) { if ((person.yearOfDeath)==null) and if ((person.yearOfDeath)==undefined)
        
-          person.yearOfDeath = new Date().getFullYear();
+//           person.yearOfDeath = new Date().getFullYear();
          
-        }
-        // If the current person is older than the currently stored oldest person, return the current person
-        if (person.yearOfDeath - person.yearOfBirth > oldestPerson.yearOfDeath - oldestPerson.yearOfBirth) {
-          return person;
-        }
-        // Otherwise, return the currently stored oldest person
-        return oldestPerson;
-      });
+//         }
+//         // If the current person is older than the currently stored oldest person, return the current person
+//         if (person.yearOfDeath - person.yearOfBirth > oldestPerson.yearOfDeath - oldestPerson.yearOfBirth) {
+//           return person;
+//         }
+//         // Otherwise, return the currently stored oldest person
+//         return oldestPerson;
+//       });
+// };
+
+// // Do not edit below this line
+// module.exports = findTheOldest;
+
+
+const findTheOldest = function(array) {
+  return array.reduce((oldest, currentPerson) => {
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+    return oldestAge < currentAge ? currentPerson : oldest;
+  })
 };
 
-// Do not edit below this line
+const getAge = function(birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+};
+
 module.exports = findTheOldest;
