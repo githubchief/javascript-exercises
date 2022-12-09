cd2638e278d4479ff1030b3687441bcd72353e10
githubchief <ranganathanshams@gmail.com>
2022-12-09 20:10:09 +0530

    add sol to findTheOldest that solves 2/3 testcase

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldestPerson, person) => {
+        // If the current person doesn't have a year of death, calculate their age as of today
+        if (!(person.yearOfDeath)) {
+       
+          person.yearOfDeath = new Date().getFullYear();
+         
+        }
+        // If the current person is older than the currently stored oldest person, return the current person
+        if (person.yearOfDeath - person.yearOfBirth > oldestPerson.yearOfDeath - oldestPerson.yearOfBirth) {
+          return person;
+        }
+        // Otherwise, return the currently stored oldest person
+        return oldestPerson;
+      });
 };
 
 // Do not edit below this line
