a24b856dd26f1b6279874c4870fe4819a3cbbc4b
githubchief <ranganathanshams@gmail.com>
2022-12-09 16:11:01 +0530

    add solutions to fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let firstNum=0;
+    let secondNum=1;
+    let thirdNum=1;
+    if(num.typeof == String) num=parseInt(num);
+    if (num<0) return "OOPS";
+   while(num)
+   {
+        thirdNum=firstNum+secondNum;
+        firstNum=secondNum;
+        secondNum=thirdNum;
+        num--;
+   }
+   return firstNum;
 };
 
 // Do not edit below this line
